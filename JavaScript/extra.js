b64d68315c8b0ef34badab4142046a962f215c5d
SyedAhsonHaider <138697651+SyedAhsonHaider@users.noreply.github.com>
2023-07-05 08:15:43 -0700

    Add files via upload

---
+++ JavaScript/extra.js
@@ -0,0 +1,38 @@
+$(".nav").animate({opacity: '0'});
+$(".ap1").hide();
+$(".cards").hide()
+var x = 1
+$(document).bind('mousewheel', function(e) {
+    $(".ap1").show();
+    $(".contacts").animate({opacity: '0'}, 0);
+    $("#iName").animate({fontSize: "24px"}, 350)
+    $("#name").animate({left: '15%', top: '10%'}, 350);
+    $('.cards').animate({opacity: '0'}, 1)
+    $(".nav").animate({opacity: '1'});
+});
+$("#letstalk").click(function(){
+    $(".ap1 h1").animate({left: '50%'}, 350);
+    $('.ap1 h1 ').attr('style','transform: translateX(-50%)');
+    $(".ap1 h1").animate({left: '50%'}, 1);
+    $(".ap1 #profile").animate({opacity: '0'}, 150);
+    $(".ap1 #pa").animate({left: '50%', width: '80%'}, 550);
+    $(".ap1 h1").html('More about me!');
+    $(".cards").show()
+
+    $('.cards').animate({opacity: '1'}, 1550)
+    var x = 2
+    $(".ap1 #pa").html("As a young boy from a middle-class family in India, I've always had a thirst for knowledge and a passion for coding. Coding has become my creative outlet and a means to express myself. Over the years, I have dedicated myself to mastering various programming languages, such as JavaScript, CSS, HTML, and Python. These languages have given me the power to bring ideas to life and solve complex problems through innovative solutions.");
+}); 
+$("#whyme").click(function(){
+    $('.cards').animate({opacity: '0'}, 1)
+    $(".ap1 #profile").animate({opacity: '0'}, 150);
+    $(".ap1 h1").animate({left: '14%'}, 350);
+    $('.ap1 h1 ').removeAttr('style','transform: translateX(-50%)')
+    $(".ap1 #pa").animate({left: '35%', width: '42%'}, 550);
+    $(".ap1 #profile").animate({opacity: '1'}, 550);
+    $(".ap1 h1").html('Hiya!, Viewer.')
+    var x = 1
+    $(".cards").hide()
+    $(".ap1 #pa").html("It is my pleasure to introduce myself as Syed Ahson Haider, a young boy hailing from a middle-class Indian family. Although my age is not disclosed, I have developed a strong interest in coding and gained proficiency in a variety of programming languages, including JavaScript, CSS, HTML, and Python. With solid 3-4 years' experience in coding and computers under my belt, I aspire to build my own company in the future. I also want to pursue a engineering career. ")
+
+});
---
+++ JavaScript/index.js
@@ -0,0 +1,31 @@
+const targetElement = document.querySelector(".name h1");
+
+if (!targetElement.classList.contains("text-effect-applied")) {
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let interval = null;
+
+  const targetValue = targetElement.dataset.value;
+  let iteration = 0;
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    targetElement.innerText = targetElement.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return targetValue[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= targetValue.length) {
+      clearInterval(interval);
+      targetElement.classList.add("text-effect-applied");
+    }
+
+    iteration += 1 / 3;
+  }, 40);
+}

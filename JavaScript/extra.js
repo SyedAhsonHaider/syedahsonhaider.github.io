b1cde016c41523781529355a25ab4f2798782c0b
SyedAhsonHaider <138697651+SyedAhsonHaider@users.noreply.github.com>
2023-07-05 08:18:01 -0700

    Update extra.js

--- JavaScript/extra.js
+++ JavaScript/extra.js
@@ -1,7 +1,6 @@
 $(".nav").animate({opacity: '0'});
 $(".ap1").hide();
 $(".cards").hide()
-var x = 1
 $(document).bind('mousewheel', function(e) {
     $(".ap1").show();
     $(".contacts").animate({opacity: '0'}, 0);
@@ -18,9 +17,7 @@ $("#letstalk").click(function(){
     $(".ap1 #pa").animate({left: '50%', width: '80%'}, 550);
     $(".ap1 h1").html('More about me!');
     $(".cards").show()
-
     $('.cards').animate({opacity: '1'}, 1550)
-    var x = 2
     $(".ap1 #pa").html("As a young boy from a middle-class family in India, I've always had a thirst for knowledge and a passion for coding. Coding has become my creative outlet and a means to express myself. Over the years, I have dedicated myself to mastering various programming languages, such as JavaScript, CSS, HTML, and Python. These languages have given me the power to bring ideas to life and solve complex problems through innovative solutions.");
 }); 
 $("#whyme").click(function(){
@@ -31,7 +28,6 @@ $("#whyme").click(function(){
     $(".ap1 #pa").animate({left: '35%', width: '42%'}, 550);
     $(".ap1 #profile").animate({opacity: '1'}, 550);
     $(".ap1 h1").html('Hiya!, Viewer.')
-    var x = 1
     $(".cards").hide()
     $(".ap1 #pa").html("It is my pleasure to introduce myself as Syed Ahson Haider, a young boy hailing from a middle-class Indian family. Although my age is not disclosed, I have developed a strong interest in coding and gained proficiency in a variety of programming languages, including JavaScript, CSS, HTML, and Python. With solid 3-4 years' experience in coding and computers under my belt, I aspire to build my own company in the future. I also want to pursue a engineering career. ")
 
